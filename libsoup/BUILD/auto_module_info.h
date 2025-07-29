78a93d05b4c38f026223d3a3f8c80bd799630aa8
Azure Linux Security Servicing Account <azurelinux-security@microsoft.com>
2025-07-29 16:21:57 +0000

    Patch libsoup for CVE-2025-4948

---
+++ libsoup/BUILD/auto_module_info.h
@@ -0,0 +1,10 @@
+#ifndef _AUTO_MODULE_INFO_H_
+#define _AUTO_MODULE_INFO_H_
+
+#define MODULE_VERSION      "3.4.4.0"
+#define PACKAGE_VERSION     "3.4.4-7.azl3"
+#define PACKAGE_NAME        "libsoup"
+#define TARGET_OS           "mariner"
+#define TARGET_OS_VERSION   "%{distro_release_version}"
+
+#endif //_AUTO_MODULE_INFO_H_
